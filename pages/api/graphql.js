34f4cb90a42d8817e1024b84208509fe1a1575a8
Iván Rodrigo <irodrigob@users.noreply.github.com>
2021-12-26 20:45:19 +0100

    develop

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,34 +1,34 @@
-import { ApolloServer, makeExecutableSchema } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 const startServer = apolloServer.start();
 
 export default async function handler(req, res) {
-  /*res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://studio.apollographql.com'
-  )
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  if (req.method === 'OPTIONS') {
-    res.end()
-    return false
-  }*/
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
 
   await startServer;
   await apolloServer.createHandler({
     path: "/api/graphql",
   })(req, res);
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
