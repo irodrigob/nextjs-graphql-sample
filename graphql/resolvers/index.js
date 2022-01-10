a63a1bd91f7dbef7c460f207450905cc198a4e95
Iván Rodrigo <irodrigob@users.noreply.github.com>
2022-01-10 22:59:00 +0100

    develop

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,7 +2,6 @@ import Product from "db/models/product.js";
 
 export const resolvers = {
   Query: {
-    // products
     getAll: async (root, args) => {
       return await Product.find({});
     },
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import { ApolloProvider } from "@apollo/client";
+import "../styles/globals.css";
+import { useApollo } from "graphql/client";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
